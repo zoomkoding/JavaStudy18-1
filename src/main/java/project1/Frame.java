ebbdf0be43c3795b5f18a024efa734b2f0dd4d5e
gur5381 <jeongjinhyeog@172.17.144.32>
2018-06-20 15:22:49 +0900

    Edit for cal in the order of operators and pressing 0 btn

--- src/main/java/project1/Frame.java
+++ src/main/java/project1/Frame.java
@@ -38,6 +38,7 @@ public class Frame {
 		label.setBorder(BorderFactory.createLineBorder(Color.RED, 2));
 		frame.add(panel);
 		JButton[] btn = new JButton[16];
+		
 		for(int i = 0 ; i<10; i++) {
 
 			btn[i] = new JButton(String.valueOf(i));
@@ -58,7 +59,7 @@ public class Frame {
 						double i = Double.parseDouble(stack.pop());
 						num += i * 10;
 						temp = String.valueOf(num);
-						while(temp.endsWith("0") || temp.endsWith(".")) {	
+						while(temp.contains(".") && (temp.endsWith("0") || temp.endsWith("."))) {	
 							temp = temp.substring(0, temp.length()-1);
 						}
 						stack.push(temp);
@@ -101,7 +102,7 @@ public class Frame {
 
 						else {
 
-							cal();
+							cal(false);
 							top = stack.pop();
 							label.setText(top);
 							stack.push(top);
@@ -116,7 +117,7 @@ public class Frame {
 
 					else if(temp.equals("+") || temp.equals("-")) {
 						pre = 1;
-						cal();
+						cal(true);
 						top = stack.pop();
 						label.setText(top);
 						stack.push(top);
@@ -127,7 +128,7 @@ public class Frame {
 					
 					
 					else if(temp.equals("=")) {
-						cal();
+						cal(true);
 						top = stack.pop();
 						label.setText(top);
 						stack.push(top);
@@ -149,18 +150,22 @@ public class Frame {
 
 	}
 
-	public void cal() {
+	public void cal(boolean go) {
 		int o = 0;
 		double num2 = Double.parseDouble(stack.pop());
 		double num1 = -1;
 		while(!stack.isEmpty()) {
 			String temp = stack.pop();
+			if((temp.equals("-") || temp.equals("+")) && go == false) {
+				stack.push(temp);
+				break;
+			}
 			if(temp.equals("+") || temp.equals("-") || temp.equals("/") || temp.equals("*")) {
 				
-				if(o == 1) num2 += num1;
-				else if(o == 2) num2 -= num1;
-				else if(o == 3) num2 *= num1;
-				else if(o == 4) num2 /= num1;
+				if(o == 1) num2 = num1 + num2;
+				else if(o == 2) num2 = num1 - num2;
+				else if(o == 3) num2 = num1 * num2;
+				else if(o == 4) num2 = num1 / num2;
 				
 				if(temp.equals("+")) o = 1;
 				else if(temp.equals("-")) o = 2;
@@ -170,8 +175,7 @@ public class Frame {
 
 
 			}
-			else num1 = Double.parseDouble(temp);
-			
+			else num1 = Double.parseDouble(temp); 
 		}
 		if(o == 1) num1 += num2;
 		else if(o == 2) num1 -= num2;
@@ -180,7 +184,7 @@ public class Frame {
 		else if(o == 0) num1 = num2;
 		
 		String temp = String.valueOf(num1);
-		while(temp.endsWith("0") || temp.endsWith(".")) {	
+		while(temp.contains(".") && (temp.endsWith("0") || temp.endsWith("."))) {	
 			temp = temp.substring(0, temp.length()-1);
 		}
 		stack.push(temp);
